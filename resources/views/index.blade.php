d95a5d1a3a6a91104567fd971eeef57b4dce018b
unknown <randygoitia7@gmail.com>
2023-06-07 21:41:18 -0400

    primer commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Neque quasi provident cum molestias amet doloremque quas beatae, labore, quos in, aperiam totam eaque voluptatibus aliquam inventore explicabo dignissimos? Voluptas, expedita.</h1>
+</body>
+</html>
