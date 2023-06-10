95f228a3b3a9bf4852b43dc5f35a942e351957e4
Maxjhon5 <jhonsor55@gmail.com>
2023-06-09 23:08:52 -0400

    modelo y controler simulador

---
+++ app/Http/Controllers/SimuladorController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Simulador;
+use Illuminate\Http\Request;
+
+class SimuladorController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Simulador $simulador)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Simulador $simulador)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Simulador $simulador)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Simulador $simulador)
+    {
+        //
+    }
+}
