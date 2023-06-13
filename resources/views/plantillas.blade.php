4102acba5028e3ea10654237bb1b8212ced9b90e
unknown <randygoitia7@gmail.com>
2023-06-13 16:37:21 -0400

    integrando el tablero

--- resources/views/plantillas.blade.php
+++ resources/views/plantillas.blade.php
@@ -5,7 +5,10 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+    
+    <link rel="stylesheet" type="text/css" href="{{asset('css/styleSimulador.css')}}">
     <link rel="stylesheet" href={{ asset('css/style-plantilla.css') }}>
+    
     <title>Document</title>
 </head>
 <body>
@@ -19,13 +22,19 @@
             <div class="miniatura">
   
             </div>
+            <div class="miniatura">
+  
+            </div>
         
             
             
         </div>
         <div class="subcontedor c2">
             <div class="hoja">
-                <p>hola mundo</p>
+                <div id="chessboard">
+                    <!-- Aquí se generará el tablero de ajedrez -->
+                </div>
+                <script src="{{ asset('js/scriptSimulador.js')}}"></script>
             </div>
         </div>
         <div class="subcontedor c3">
