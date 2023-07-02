ac17cedefe344abe9f07bcba28e4003a973350a8
unknown <randygoitia7@gmail.com>
2023-07-02 15:25:12 -0400

    modificando camino

--- resources/views/visualizador.blade.php
+++ resources/views/visualizador.blade.php
@@ -15,7 +15,7 @@
     <script src="https://code.jquery.com/jquery-3.3.1.min.js"
         integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
     <link rel="stylesheet" type="text/css" href="{{ asset('css/styleSimulador.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/style-plantilla.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/style-visualizador.css') }}">
 
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.6.0/jszip.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/FileSaver.js/2.0.5/FileSaver.min.js"></script>
@@ -38,22 +38,26 @@
     @endif
     <br>
     <div class="card">
+        <button onclick="cargarScriptDinamico()">cargar</button>
 
         <div class="card-body">
             <p class="h4">Paginas del curso.-</p>
 
             @foreach ($curso_paginas as $curso_pagina)
-                <div>
-
-                    <label class="form-check-label" for="flexCheckDefault">
-                        {{ $curso_pagina->nombre_pagina }}
-                    </label>
-                    <iframe width="1000" height="1000" frameborder="0"
+                <div class="contenedor">            
+                    <iframe width="900px" height="900px" frameborder="0"
                         srcdoc='    
                     <html>
                     <head>
+                        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+
+
+
                     <link rel="stylesheet" type="text/css" href="{{ asset('css/styleSimulador.css') }}">
                     <link rel="stylesheet" href="{{ asset('css/style-visualizador.css') }}">
+                    <script src="https://code.jquery.com/jquery-3.3.1.min.js"
+                    integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
                     </head>
                     <body>
                         <div class="container">
@@ -63,7 +67,30 @@
                             </div>
                         </div>
                     </div>
-                   
+                    <div id="myModal" class="modal">
+                        <div class="modal-content">
+                            <span class="close">&times;</span>
+                            <div class="modal-header">
+            
+                                <h2>¡Objetivo alcanzado!</h2>
+                            </div>
+                            <div class="modal-body">
+                                <p class="textoNumeroDePasos" id="textoNumeroDePasos"></p>
+                            </div>
+                        </div>
+                    </div>
+                    
+                    <script src="{{ asset('js/visualizador.js') }}"></script>
+                    <script>                                       
+                        $(document).ready(function() {                                        
+                            $(document).on("click", "#BotonIniciar", function() {
+                                // Código para el evento click del contenido dinámico
+                                console.log("¡Haz hecho clic en el botón!");
+                                cargarScriptDinamico();
+                
+                            });                                                                             
+                        });
+                    </script>
                     </body>
                     </html>'></iframe>
 
